7645e3689ca86cab6f3cfb584176fb0ed9ef9cd8
Thiago <Thiagofelicio@hotmail.com>
2018-08-02 19:04:06 -0300

    Changes ABC

--- src/main/java/sample/ABC2.java
+++ src/main/java/sample/ABC2.java
@@ -1,25 +1,37 @@
 package sample;
 
-import java.io.IOException;
-import java.util.Dictionary;
-import java.util.HashMap;
-import java.util.Hashtable;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
 
-public class ABC2 {
+import javax.xml.bind.DatatypeConverter;
+import java.io.*;
+import java.lang.reflect.Type;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.sql.*;
+import java.util.*;
+
+class ABC2 {
+
     public static void main(String args[]) throws IOException, InterruptedException {
 
-        // create a new hasthtable
-        Dictionary d = new Hashtable();
+        ServerSocket Servidor = new ServerSocket(7071);
+        Socket C2 = Servidor.accept();
+        InputStream is = C2.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
 
-        // put some elements
-        d.put("1", "Chocolate");
-        d.put("2", "Cocoa");
+        String message = br.readLine();
 
-        d.put("5", "Coffee");
-        d.put("5", "Coffee2");
+        System.out.println("Message received from the client : " + message);
+        String image = message.split(";")[3];
+        byte[] decoded = DatatypeConverter.parseBase64Binary("aGVsbG8gd29ybGQ=");
+        System.out.println(new String(decoded, "UTF-8"));
 
-        // print how many times put was called
-        System.out.println(d);
 
     }
+
+
 }
